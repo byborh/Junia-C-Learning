e255f72d34c901a079a5744ee92eec39170284d1
byborh <byborh@gmail.com>
2025-10-08 21:56:38 +0200

    exercice 5

--- string/exercice-5.c
+++ string/exercice-5.c
@@ -9,6 +9,28 @@ int main() {
         Écrire un programme qui permet de déterminer si une suite de caractères est un palindrome. Il
         devra pouvoir traiter des mots autant que des phrases.
     */
+
+    
+    char palindrome[100];
+
+
+    printf("Veuillez saisir quelque chose: \n");
+    fgets(palindrome, sizeof(palindrome), stdin);
+
+    int palindrome_length = strlen(palindrome);
+
+
+    // Supprimer les sauts de lignes 
+    
+
+    for(int i = 0; i < palindrome_length / 2; i++) {
+        if(palindrome[i] != palindrome[palindrome_length - 1 - i]) {
+            printf("C'est pas un mot palindrome !");
+            return 0;
+        }
+    }
+
+    printf("C'est un mot palindrome !!!");
     
     return 0;
 }
