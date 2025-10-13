253db55db797cd16ef935e1e4cb645aaae4d47bf
byborh <byborh@gmail.com>
2025-10-13 11:24:50 +0200

    pointer/returnMinMax
    Signed-off-by: byborh <byborh@gmail.com>

---
+++ pointer/returnMinMax.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+void returnMinMaxOfTable(int tab[], int* min, int *max) {
+    
+    for(int i = 0; i < 5; i++) {
+        if(*max < tab[i]) *max = tab[i];
+        if(*min > tab[i]) *min = tab[i];
+    }
+};
+
+int main() {
+    int min = INT_MAX;
+    int max = INT_MIN;
+
+    int* pMin = &min;
+    int* pMax = &max;
+
+    int tab[5] = {4, 5, 78, 1, 77};
+
+    returnMinMaxOfTable(tab, pMin, pMax);
+
+    printf("Voici le minimum du tableau: %d et voici donc le maximum: %d\n", min, max);
+
+    return 0;
+}
