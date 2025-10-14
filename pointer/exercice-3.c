e23a589e6b29070fe119fac56ce68979e1bdb8b7
byborh <byborh@gmail.com>
2025-10-14 10:05:01 +0200

    pointer/exercice3

--- pointer/exercice-3.c
+++ pointer/exercice-3.c
@@ -1,7 +1,40 @@
 #include <stdio.h>
+#include <limits.h>
+
+/*    
+    Écrire une fonction qui détermine les indices de la plus grande et la plus petite valeur dans un
+    tableau d'entiers : int maxima (int tab[], int n, int *imax, int * imin);
+    La fonction retourne dans imax et imin les indices chercher.
+    2
+    Si le tableau contient plusieurs maxima ou minima, la fonction retiendra la position du premier
+    maximum ou minimum rencontré.
+    Écrire un programme qui affiche ensuite la valeur et la position du maximum et du minimum.
+*/
+
+void indexOfMinMax(int tab[5], int* indexMin, int* indexMax) {
+    int min = INT_MAX;
+    int max = INT_MIN;
+    for(int i = 0; i < 5; i++) {
+        if(min > tab[i]) {
+            min = tab[i];
+            *indexMin = i; 
+        }
+        if(max < tab[i]) {
+            max = tab[i];
+            *indexMax = i;
+        }
+    }
+}
+
 
 int main() {
+    int tab[5] = {45, 2, 89, 17, 35};
 
+    int indexMin = 0, indexMax = 0;
+
+    indexOfMinMax(tab, &indexMin, &indexMax);
+
+    printf("Voici l'index du minimum du tableau: %d et voici donc l'index du maximum: %d\n", indexMin, indexMax);
 
     return 0;
 }
--- pointer/exercice-4.c
+++ pointer/exercice-4.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <math.h>
+
+
+int quadraticEquation(int a, int b, int c, float *x1, float *x2);
 
 int main() {
-
-
-    return 0;
+ 
 }
