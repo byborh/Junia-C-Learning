e51448eae62b53c062a1fb42bbaeab28784f8554
byborh <byborh@gmail.com>
2025-11-26 18:26:58 +0100

    structure/exercice-1

--- structure/exercice-1.c
+++ structure/exercice-1.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 
-typedef struct P {
-    char* Nom;
-    char* Prenom;
-    int* DateDeNaissance;  
-    char* Matricule;
-}
+typedef struct {
+    char nom[30];
+    char prenom[30];
+    int dateDeNaissance[3];  
+    char matricule[20];
+} personne;
 
 
-int main() {
-    struct P personne;
-    personne.Prenom = "Antony";
-    personne.Nom = "Junita";
-    personne.DateDeNaissance = 15072005;
+int main(void) {
+    personne P = {"Antony", "Junita", {10, 06, 2003}, "A23"};
 
-    printf("")
+    printf("Voici la personne que je viens de créer :\n");
+    printf(" nom: %s, prenom: %s, date: %02d/%02d/%d, matricule: %s\n",
+           P.nom, P.prenom,
+           P.dateDeNaissance[0], P.dateDeNaissance[1], P.dateDeNaissance[2],
+           P.matricule);
+
+    return 0;
 }
